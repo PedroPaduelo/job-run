530ef7a3080ba455c632e5f5a7083ccdebf138fd
Sandbox User <sandbox@nommand.local>
2026-05-10 21:56:12 +0000

    asdfasdf

--- frontend-job-async/vite.config.ts
+++ frontend-job-async/vite.config.ts
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 4173,
+    port: 3000,
     allowedHosts: 'all',
     cors: true,
   },
   preview: {
     host: '0.0.0.0',
-    port: 4173,
+    port: 3000,
     allowedHosts: 'all',
     cors: true,
   },
